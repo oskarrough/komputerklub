b8cb6d2c5e5ed453978ec6e82496704041172871
Oskar Rough <oskarrough@gmail.com>
2013-02-08 23:47:09 +0100

    Map model to Drupal API

--- app/scripts/models/estate-model.js
+++ app/scripts/models/estate-model.js
@@ -13,6 +13,19 @@ define([
       price: "undefined",
       size: "undefined",
       rooms: "undefined"
+    },
+
+    parse: function(response) {
+      response.image = response.field_image;
+      response.price = response.field_price;
+      response.size = response.field_size;
+      response.rooms = response.field_rooms;
+      response.image = response.field_image;
+      // @todo: not sure where this check should be made? Oskar.
+      // if(response.image.length < 1) {
+      //   response.image = 'No image';
+      // }
+      return response;
     }
 
   });
--- app/scripts/views/estateList-view.js
+++ app/scripts/views/estateList-view.js
@@ -9,23 +9,22 @@ function(app, Backbone, estateListTemplate) {
   var self,
   EstateListView = Backbone.View.extend({
 
-    // Represents the actual DOM element that corresponds to your View (There is a one to one relationship between View Objects and DOM elements)
+    // Represents the actual DOM element that corresponds to your View
+    // (There is a one to one relationship between View Objects and DOM elements)
     el: '#main',
     template: _.template(estateListTemplate),
 
     // View constructor
     initialize: function() {
 
-        // Storing the View context
-        self = this;
-
-        this.collection.bind("filter", this.render, this);
+      // Storing the View context
+      self = this;
+      this.collection.bind("filter", this.render, this);
     },
+    
     render: function() {
-console.log('render');
-        self.$el.html(_.template(estateListTemplate, {estateCollection: this.collection.models}));
-
-        return self;
+      self.$el.html(_.template(estateListTemplate, { estateCollection: this.collection.models }));
+      return self;
     }
   });
 
